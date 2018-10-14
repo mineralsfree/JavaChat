bee33dde33db537d3f84b89a74b0ccdbd62b7bcb
Nikita Pilinko <mineralsfree@gmail.com>
2018-10-14 20:14:35 +0300

    added ID support into javaScript
    Feel sad

--- Web-chat/data.js
+++ Web-chat/data.js
@@ -3,21 +3,26 @@ class Message {
         this.user = user;
         this.message = message
         this.mt = mt;
+
     }
     getUserType(){
         return this.user.getType();
     }
 }
 class User {
-    constructor (name,type){
+    constructor (name,type,id){
         this.name = name;
         this.type = type;
+        this.id =id;
     }
     getName(){
         return this.name;
     }
     getType(){
         return this.type;
+    }
+    setID(id){
+        this.id = id;
     }
 
 
